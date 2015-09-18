04a32e3698755f9a56d2242c1f2b945863deb23c
Max Maton <max@maton.info>
2015-09-18 13:30:10 +0200

    Fix failing bullet unit test
    <testing, bug, logic

--- src/test/java/nl/delftelectronics/spaceinvaders/core/entities/BulletTest.java
+++ src/test/java/nl/delftelectronics/spaceinvaders/core/entities/BulletTest.java
@@ -22,22 +22,22 @@ public class BulletTest extends TestCase {
      *{@link nl.delftelectronics.spaceinvaders.core.entities.Bullet#update(org.joda.time.Interval)}.
      */
     public void testUpdate() {
-        final int dimension = 10;
-        final int maximumInterval = 1000;
+        final double dimension = 10;
 
         GameScene scene = mock(GameScene.class);
         when(scene.getCollisions(any(Collidable.class))).thenReturn(new ArrayList<Entity>());
         
+        // Moving speed is per milisecond
         Bullet bullet = new Bullet(dimension, dimension, dimension, dimension, true);
         bullet.initialize(scene);
-        Assert.assertEquals((long) dimension, (long) bullet.getPositionY());
-        bullet.update(new Interval(0, maximumInterval));
-        Assert.assertEquals((long) dimension - Bullet.MOVING_SPEED, (long) bullet.getPositionY());
+        Assert.assertEquals(dimension, bullet.getPositionY());
+        bullet.update(new Interval(0, 1));
+        Assert.assertEquals(dimension - Bullet.MOVING_SPEED, bullet.getPositionY());
 
         Bullet down = new Bullet(dimension, dimension, dimension, dimension, false);
         down.initialize(scene);
-        Assert.assertEquals((long) dimension, (long) down.getPositionY());
-        down.update(new Interval(0, maximumInterval));
-        Assert.assertEquals((long) dimension + Bullet.MOVING_SPEED, (long) down.getPositionY());
+        Assert.assertEquals(dimension, down.getPositionY());
+        down.update(new Interval(0, 1));
+        Assert.assertEquals(dimension + Bullet.MOVING_SPEED, down.getPositionY());
     }
 }
