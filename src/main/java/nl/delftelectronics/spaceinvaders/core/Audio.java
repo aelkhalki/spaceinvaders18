5d3c99aa138aca5992e1f53e1b271ddd87754128
Ali Smesseim <smesseim.ali@gmail.com>
2015-09-25 22:26:53 +0200

    Add JavaDoc to Audio
    <documentation

--- src/main/java/nl/delftelectronics/spaceinvaders/core/Audio.java
+++ src/main/java/nl/delftelectronics/spaceinvaders/core/Audio.java
@@ -2,38 +2,48 @@ package nl.delftelectronics.spaceinvaders.core;
 
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.util.Duration;
-
-import java.io.File;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 
 /**
- * Created by ali on 9/25/15.
+ * Used to play music and sound effects.
  */
-public class Audio {
+public final class Audio {
     private static Collection<MediaPlayer> mps = new ArrayList<MediaPlayer>();
 
     private static String bombSoundFilename = "/bomb_sound.mp3";
     private static String backgroundSoundFilename = "/background_sound.mp3";
 
+    /**
+     * Play the bullet sound effect.
+     */
     public static void playBulletSound() {
         playSound(bombSoundFilename);
     }
 
+    /**
+     * Play the bomb sound effect.
+     */
     public static void playBombSound() {
         playSound(bombSoundFilename);
     }
 
+    /**
+     * Play the background music.
+     */
     public static void playBackgroundSound() {
         playSound(backgroundSoundFilename);
     }
 
+    /**
+     * Play a sound from a file.
+     * @param filename filename of the sound.
+     */
     private static void playSound(final String filename) {
         Media media = new Media(Audio.class.getResource(filename).toString());
         MediaPlayer mp = new MediaPlayer(media);
         mp.play();
+        // Add Media Player to Collection so that it doesn't get garbage collected.
         mps.add(mp);
     }
 }
