80d0d103c5196889a3041557a196dc547b1da85b
Sander Oostmeyer <ictpanel89@gmail.com>
2015-09-25 22:56:47 +0200

    Code clean up

--- src/test/java/nl/delftelectronics/spaceinvaders/core/entities/MediumEnemyTest.java
+++ src/test/java/nl/delftelectronics/spaceinvaders/core/entities/MediumEnemyTest.java
@@ -5,51 +5,47 @@ import junit.framework.TestCase;
 
 public class MediumEnemyTest extends TestCase {
 
-	MediumEnemy enemy = new MediumEnemy(10, 10, 10, 10,10, 10, 0 ,null);
+	MediumEnemy enemy = new MediumEnemy(10, 10, 10, 10, 10, 10, 0, null);
 	int speed = enemy.MOVE_DOWN_SPEED;
-	
-	public void testmediumEnemy(){
-		
-	MediumEnemy mediumenemy = new MediumEnemy(10, 10, 10, 10,10, 10, 0,null);
-	
-	Assert.assertEquals(false, mediumenemy.isKilled);
-	
-	Assert.assertEquals(10.0, mediumenemy.getPositionX());
-	Assert.assertEquals(10.0, mediumenemy.getPositionY());
-	
+
+	public void testmediumEnemy() {
+
+		MediumEnemy mediumenemy = new MediumEnemy(10, 10, 10, 10, 10, 10, 0,null);
+
+		Assert.assertEquals(false, mediumenemy.isKilled);
+		Assert.assertEquals(10.0, mediumenemy.getPositionX());
+		Assert.assertEquals(10.0, mediumenemy.getPositionY());
+
 	}
-	
-	public void testmediumEnemyMovement(){
-		
-	MediumEnemy mediumenemy = new MediumEnemy(10, speed * 2, 10, 10,10, 10, 0 ,null);
-	
-	double x = mediumenemy.getPositionY();
-	
-	mediumenemy.moveDown();
-	
-	Assert.assertEquals(x+mediumenemy.MOVE_DOWN_SPEED, mediumenemy.getPositionY());	
-	
+
+	public void testmediumEnemyMovement() {
+
+		MediumEnemy mediumenemy = new MediumEnemy(10, speed * 2, 10, 10, 10,10, 0, null);
+		double x = mediumenemy.getPositionY();
+
+		mediumenemy.moveDown();
+		Assert.assertEquals(x + mediumenemy.MOVE_DOWN_SPEED,mediumenemy.getPositionY());
+
 	}
-	
-	public void testmediumEnemyKill(){
+
+	public void testmediumEnemyKill() {
+
+		MediumEnemy mediumenemy = new MediumEnemy(10, 10, 10, 10, 10, 10, 0,null);
 		
-		MediumEnemy mediumenemy = new MediumEnemy(10, 10, 10, 10,10, 10, 0,null);
 		Assert.assertEquals(false, mediumenemy.isKilled);
 		mediumenemy.kill();
 		Assert.assertEquals(true, mediumenemy.isKilled);
 
-		
 	}
-	
-	public void testmediumEnemyReachedBottom(){
-		
-		MediumEnemy mediumenemy = new MediumEnemy(0, 40 , 10, 10,10, 10, 180,null);
-		
+
+	public void testmediumEnemyReachedBottom() {
+
+		MediumEnemy mediumenemy = new MediumEnemy(0, 40, 10, 10, 10, 10, 180,null);
+
 		mediumenemy.moveDown();
 		Assert.assertEquals(false, mediumenemy.reachedBottom());
 		mediumenemy.moveDown();
 		Assert.assertEquals(true, mediumenemy.reachedBottom());
 	}
-	
-	
+
 }
--- src/test/java/nl/delftelectronics/spaceinvaders/core/entities/SmallEnemyTest.java
+++ src/test/java/nl/delftelectronics/spaceinvaders/core/entities/SmallEnemyTest.java
@@ -4,52 +4,48 @@ import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class SmallEnemyTest extends TestCase {
-	
-	SmallEnemy enemy = new SmallEnemy(10, 10, 10, 10,10, 10, 0 ,null);
+
+	SmallEnemy enemy = new SmallEnemy(10, 10, 10, 10, 10, 10, 0, null);
 	int speed = enemy.MOVE_DOWN_SPEED;
-	
-	public void testSmallEnemy(){
-		
-	SmallEnemy smallenemy = new SmallEnemy(10, 10, 10, 10,10, 10, 0,null);
-	
-	Assert.assertEquals(false, smallenemy.isKilled);
-	
-	Assert.assertEquals(10.0, smallenemy.getPositionX());
-	Assert.assertEquals(10.0, smallenemy.getPositionY());
-	
+
+	public void testSmallEnemy() {
+
+		SmallEnemy smallenemy = new SmallEnemy(10, 10, 10, 10, 10, 10, 0, null);
+
+		Assert.assertEquals(false, smallenemy.isKilled);
+		Assert.assertEquals(10.0, smallenemy.getPositionX());
+		Assert.assertEquals(10.0, smallenemy.getPositionY());
+
 	}
-	
-	public void testSmallEnemyMovement(){
+
+	public void testSmallEnemyMovement() {
+
+		SmallEnemy smallenemy = new SmallEnemy(10, speed * 2, 10, 10, 10, 10,0, null);
+		double x = smallenemy.getPositionY();
 		
-	SmallEnemy smallenemy = new SmallEnemy(10, speed * 2, 10, 10,10, 10, 0 ,null);
-	
-	double x = smallenemy.getPositionY();
-	
-	smallenemy.moveDown();
-	
-	Assert.assertEquals(x+smallenemy.MOVE_DOWN_SPEED, smallenemy.getPositionY());	
-	
+		smallenemy.moveDown();
+		Assert.assertEquals(x + smallenemy.MOVE_DOWN_SPEED,smallenemy.getPositionY());
+
 	}
-	
-	public void testSmallEnemyKill(){
+
+	public void testSmallEnemyKill() {
+
+		SmallEnemy smallenemy = new SmallEnemy(10, 10, 10, 10, 10, 10, 0, null);
 		
-		SmallEnemy smallenemy = new SmallEnemy(10, 10, 10, 10,10, 10, 0,null);
 		Assert.assertEquals(false, smallenemy.isKilled);
 		smallenemy.kill();
 		Assert.assertEquals(true, smallenemy.isKilled);
 
-		
 	}
-	
-	public void testSmallEnemyReachedBottom(){
-		
-		SmallEnemy smallenemy = new SmallEnemy(0, 40 , 10, 10,10, 10, 180,null);
-		
+
+	public void testSmallEnemyReachedBottom() {
+
+		SmallEnemy smallenemy = new SmallEnemy(0, 40, 10, 10, 10, 10, 180, null);
+
 		smallenemy.moveDown();
 		Assert.assertEquals(false, smallenemy.reachedBottom());
 		smallenemy.moveDown();
 		Assert.assertEquals(true, smallenemy.reachedBottom());
 	}
-	
-	
+
 }
